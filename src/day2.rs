64316e3084390db310841f02e7c8fe9adfa253dc
Michael Vesterli <michael@vesterli.dk>
2023-12-02 09:02:23 +0100

    Day 2 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -34,3 +34,30 @@ fn part1() {
     }
     println!("{sum}");
 }
+
+#[test]
+fn part2() {
+    let input = include_str!("../input/day2.txt");
+
+    let mut sum = 0;
+    for game in input.lines() {
+        let (_, game) = game.split_once(':').unwrap();
+
+        let (mut max_r, mut max_g, mut max_b) = (0, 0, 0);
+        for grab in game.split(';') {
+            for color in grab.split(',').map(str::trim) {
+                let (num, color) = color.split_once(' ').unwrap();
+                let num: i32 = num.parse().unwrap();
+
+                match color {
+                    "red" => max_r = max_r.max(num),
+                    "green" => max_g = max_g.max(num),
+                    "blue" => max_b = max_b.max(num),
+                    _ => unreachable!(),
+                };
+            }
+        }
+        sum += max_r * max_g * max_b;
+    }
+    println!("{sum}");
+}
