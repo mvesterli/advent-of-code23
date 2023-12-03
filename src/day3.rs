7f1fc39b0ef97b8ed6e6583402605ae667467d1d
Michael Vesterli <michael@vesterli.dk>
2023-12-03 14:58:08 +0100

    Day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -37,3 +37,33 @@ fn part1() {
 
     println!("{sum}");
 }
+
+#[test]
+fn part2() {
+    let input = include_str!("../input/day3.txt");
+
+    let re = Regex::new(r"\d+").unwrap();
+    let nums: Vec<Vec<_>> = input.lines().map(|l| re.find_iter(l).collect()).collect();
+    let chars: Vec<Vec<_>> = input.lines().map(|l| l.chars().collect()).collect();
+
+    let mut sum = 0;
+    for (row, line) in chars.iter().enumerate() {
+        for (col, c) in line.iter().enumerate() {
+            if *c == '*' {
+                let mut parts: Vec<i64> = Vec::new();
+                for row in row.saturating_sub(1)..=row + 1 {
+                    for num in &nums[row] {
+                        if num.start() <= col + 1 && num.end() >= col {
+                            parts.push(num.as_str().parse().unwrap());
+                        }
+                    }
+                }
+                if parts.len() == 2 {
+                    sum += parts[0] * parts[1];
+                }
+            }
+        }
+    }
+
+    println!("{sum}");
+}
