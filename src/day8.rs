6ba2f6de30949bb605f981280ab50ab4f3884861
Michael Vesterli <michael@vesterli.dk>
2023-12-08 20:14:25 +0100

    Day 8 part 2

--- src/day8.rs
+++ src/day8.rs
@@ -32,3 +32,58 @@ fn part1() {
     }
     println!("{}", res);
 }
+
+#[test]
+fn part2() {
+    let input = include_str!("../input/day8.txt");
+
+    let mut lines = input.lines();
+    let steps: Vec<_> = lines.next().unwrap().chars().collect();
+    lines.next();
+
+    let re = Regex::new(r"(\w+) = \((\w+), (\w+)\)").unwrap();
+    let input: Vec<[&str; 3]> = lines
+        .map(|line| re.captures(line).unwrap().extract().1)
+        .collect();
+
+    let mut node_to_id = HashMap::new();
+    for line in &input {
+        node_to_id.insert(line[0], node_to_id.len());
+    }
+
+    let mut neighbors = vec![(0, 0); node_to_id.len()];
+    for line in &input {
+        neighbors[node_to_id[line[0]]] = (node_to_id[line[1]], node_to_id[line[2]]);
+    }
+
+    let starts: Vec<_> = input
+        .iter()
+        .enumerate()
+        .filter(|v| v.1[0].ends_with('A'))
+        .map(|v| v.0)
+        .collect();
+
+    let is_end: Vec<_> = input.iter().map(|v| v[0].ends_with('Z')).collect();
+
+    let mut results = Vec::new();
+    for start in starts {
+        let mut node = start;
+        let mut step = 0;
+        let mut dist = 0;
+        while !is_end[node] {
+            node = if steps[step] == 'L' {
+                neighbors[node].0
+            } else {
+                neighbors[node].1
+            };
+            step = (step + 1) % steps.len();
+            dist += 1;
+        }
+        // Apparently it was not necessary to handle looping around to
+        // multiple end nodes or the same one multiple times
+        results.push(dist);
+    }
+
+    let res = results.into_iter().fold(1i64, num::integer::lcm);
+    println!("{res}");
+}
