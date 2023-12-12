0531a358d2c220f28883627ec3e1c784b72dd6f5
Michael Vesterli <michael@vesterli.dk>
2023-12-12 22:26:59 +0100

    Day 12 part 2

--- src/day12.rs
+++ src/day12.rs
@@ -1,45 +1,58 @@
 #![cfg(test)]
 
-fn try_place(row: &[char], num: usize) -> Option<&[char]> {
-    if row[0..num].iter().any(|v| *v == '.') {
-        return None;
-    }
-    if row.len() == num {
-        Some(&[])
-    } else {
-        if row[num] == '#' {
-            None
-        } else {
-            Some(&row[num + 1..])
-        }
-    }
+struct Input {
+    row: Vec<char>,
+    nums: Vec<usize>,
 }
 
-fn num_options(row: &[char], placed: &[usize]) -> u32 {
-    if placed.is_empty() {
-        if row.contains(&'#') {
-            0
-        } else {
-            1
+impl Input {
+    fn try_place(&self, row_idx: usize, num: usize) -> Option<usize> {
+        if row_idx + num > self.row.len()
+            || self.row[row_idx..row_idx + num].iter().any(|v| *v == '.')
+        {
+            return None;
         }
-    } else if row.len() < placed[0] {
-        0
-    } else {
-        if row[0] == '#' {
-            if let Some(rest) = try_place(row, placed[0]) {
-                num_options(rest, &placed[1..])
+        if row_idx + num == self.row.len() {
+            Some(row_idx + num)
+        } else {
+            if self.row[row_idx + num] == '#' {
+                None
+            } else {
+                Some(row_idx + num + 1)
+            }
+        }
+    }
+
+    fn num_options(&self, dp: &mut Vec<Vec<Option<u64>>>, row_idx: usize, num_idx: usize) -> u64 {
+        if let Some(res) = dp[row_idx][num_idx] {
+            return res;
+        }
+
+        let res = if num_idx >= self.nums.len() {
+            if self.row[row_idx..].contains(&'#') {
+                0
+            } else {
+                1
+            }
+        } else if row_idx + self.nums[num_idx] > self.row.len() {
+            0
+        } else if self.row[row_idx] == '#' {
+            if let Some(next_idx) = self.try_place(row_idx, self.nums[num_idx]) {
+                self.num_options(dp, next_idx, num_idx + 1)
             } else {
                 0
             }
-        } else if row[0] == '?' {
-            let mut res = num_options(&row[1..], placed);
-            if let Some(rest) = try_place(row, placed[0]) {
-                res += num_options(rest, &placed[1..]);
+        } else if self.row[row_idx] == '?' {
+            let mut res = self.num_options(dp, row_idx + 1, num_idx);
+            if let Some(next_idx) = self.try_place(row_idx, self.nums[num_idx]) {
+                res += self.num_options(dp, next_idx, num_idx + 1);
             }
             res
         } else {
-            num_options(&row[1..], placed)
-        }
+            self.num_options(dp, row_idx + 1, num_idx)
+        };
+        dp[row_idx][num_idx] = Some(res);
+        res
     }
 }
 
@@ -48,13 +61,33 @@ fn part1() {
     let input = include_str!("../input/day12.txt");
 
     let mut sum = 0;
-
     for line in input.lines() {
         let (row, check) = line.split_once(' ').unwrap();
         let row: Vec<char> = row.chars().collect();
-        let check: Vec<usize> = check.split(',').filter_map(|v| v.parse().ok()).collect();
-        sum += num_options(&row, &check);
-    }
+        let nums: Vec<usize> = check.split(',').filter_map(|v| v.parse().ok()).collect();
 
+        let mut dp = vec![vec![None; nums.len() + 1]; row.len() + 1];
+        let input = Input { row, nums };
+        sum += input.num_options(&mut dp, 0, 0);
+    }
+    println!("{sum}");
+}
+
+#[test]
+fn part2() {
+    let input = include_str!("../input/day12.txt");
+
+    let mut sum = 0;
+    for line in input.lines() {
+        let (row, check) = line.split_once(' ').unwrap();
+        let row = format!("{}?{}?{}?{}?{}", row, row, row, row, row);
+        let check = format!("{},{},{},{},{}", check, check, check, check, check);
+        let row: Vec<char> = row.chars().collect();
+        let nums: Vec<usize> = check.split(',').filter_map(|v| v.parse().ok()).collect();
+
+        let mut dp = vec![vec![None; nums.len() + 1]; row.len() + 1];
+        let input = Input { row, nums };
+        sum += input.num_options(&mut dp, 0, 0);
+    }
     println!("{sum}");
 }
