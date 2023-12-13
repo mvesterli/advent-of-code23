4a6d8cc3ddad55d486e4494646c482ccdef54339
Michael Vesterli <michael@vesterli.dk>
2023-12-13 21:47:07 +0100

    Day 13 part 2

--- src/day13.rs
+++ src/day13.rs
@@ -41,11 +41,46 @@ fn get_score_1((rows, cols): (Vec<u32>, Vec<u32>)) -> usize {
     0
 }
 
+fn get_score_2((rows, cols): (Vec<u32>, Vec<u32>)) -> usize {
+    for i in 1..rows.len() {
+        let is_mirror = (0..i)
+            .rev()
+            .zip(i..rows.len())
+            .map(|(a, b)| (rows[a] ^ rows[b]).count_ones())
+            .sum::<u32>()
+            == 1;
+        if is_mirror {
+            return 100 * i;
+        }
+    }
+    for i in 1..cols.len() {
+        let is_mirror = (0..i)
+            .rev()
+            .zip(i..cols.len())
+            .map(|(a, b)| (cols[a] ^ cols[b]).count_ones())
+            .sum::<u32>()
+            == 1;
+        if is_mirror {
+            return i;
+        }
+    }
+    0
+}
+
 #[test]
 fn part1() {
     let input = include_str!("../input/day13.txt");
     let lines: Vec<_> = input.lines().collect();
     let patterns = lines.split(|s| s.is_empty());
     let sum: usize = patterns.map(parse_bin).map(get_score_1).sum();
+    println!("{sum}");
+}
+
+#[test]
+fn part2() {
+    let input = include_str!("../input/day13.txt");
+    let lines: Vec<_> = input.lines().collect();
+    let patterns = lines.split(|s| s.is_empty());
+    let sum: usize = patterns.map(parse_bin).map(get_score_2).sum();
     println!("{sum}");
 }
