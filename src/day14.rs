0477151d0b3242cfedac806e6599d228d3176397
Michael Vesterli <michael@vesterli.dk>
2023-12-14 19:12:44 +0100

    Day 14 part 2

--- src/day14.rs
+++ src/day14.rs
@@ -1,7 +1,7 @@
 #![cfg(test)]
 
 fn tilt_north(grid: &mut Vec<Vec<char>>) {
-    for col_idx in (0..grid[0].len()) {
+    for col_idx in 0..grid[0].len() {
         let mut next_empty = 0;
         for row_idx in 0..grid.len() {
             match grid[row_idx][col_idx] {
@@ -19,6 +19,25 @@ fn tilt_north(grid: &mut Vec<Vec<char>>) {
     }
 }
 
+fn turn_right(grid: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut res = grid.clone();
+    for row in 0..res.len() {
+        for col in 0..res[0].len() {
+            res[row][col] = grid[res.len() - 1 - col][row];
+        }
+    }
+    res
+}
+
+fn cycle(grid: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut res = grid.clone();
+    for _ in 0..4 {
+        tilt_north(&mut res);
+        res = turn_right(res);
+    }
+    res
+}
+
 fn load(grid: &Vec<Vec<char>>) -> usize {
     grid.iter()
         .enumerate()
@@ -32,4 +51,26 @@ fn part1() {
     let mut input: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
     tilt_north(&mut input);
     println!("{}", load(&input));
+}
+
+#[test]
+fn part2() {
+    let input = include_str!("../input/day14.txt");
+    let mut input: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
+
+    let mut results = Vec::new();
+    for current_cycle in 0.. {
+        let next = cycle(&input);
+        for (idx, grid) in results.iter().enumerate() {
+            if next == *grid {
+                let loopback_len = current_cycle - idx + 1;
+                if (1000_000_000 - current_cycle) % loopback_len == 0 {
+                    println!("{}", load(&input));
+                    return;
+                }
+            }
+        }
+        results.push(input);
+        input = next;
+    }
 }
